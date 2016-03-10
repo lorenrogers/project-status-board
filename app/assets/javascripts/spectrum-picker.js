0d9fd70a046a069cdcea50cfd0fe738cad4b1d32
Loren Rogers <loren@lorentrogers.com>
2016-03-09 20:55:41 -0500

    Checkin

--- app/assets/javascripts/spectrum-picker.js
+++ app/assets/javascripts/spectrum-picker.js
@@ -4,6 +4,15 @@ $(document).ready(function(){
     flat: true,
     showInput: true,
     allowEmpty:true,
-    showButtons: false
+    showButtons: false,
+    showPalette:true,
+    palette:
+      [
+        [ "#3182bd", "#6baed6", "#9ecae1", "#c6dbef" ],
+        [ "#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2" ],
+        [ "#31a354", "#74c476", "#a1d99b", "#c7e9c0" ],
+        [ "#756bb1", "#9e9ac8", "#bcbddc", "#dadaeb" ],
+        [ "#636363", "#969696", "#bdbdbd", "#d9d9d9" ]
+      ]
   });
 });
